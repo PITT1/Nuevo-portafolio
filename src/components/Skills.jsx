15cebdbfc82345f7967b276a2c52f654ba511559
PITT1 <petroleo.plta.97@gmail.com>
2024-06-24 11:44:19 -0400

    empezando con la seccion skills

---
+++ src/components/Skills.jsx
@@ -0,0 +1,15 @@
+import '../styles/skills-styles.css';
+
+const Skills = () => {
+    return(
+        <section className='container-skills'>
+            <h1>Skills</h1>
+            <div className='container2-skills'>
+                <div className='window'></div>
+                <div className='carousel'></div>
+            </div>
+        </section>
+    )
+}
+
+export default Skills;
