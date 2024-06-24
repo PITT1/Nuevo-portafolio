d27de8f9f083b4825f0b8a7a29b08409cb0c58fa
PITT1 <petroleo.plta.97@gmail.com>
2024-06-24 13:20:52 -0400

    creando la ventana de la macbook

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,7 +5,15 @@ const Skills = () => {
         <section className='container-skills'>
             <h1>Skills</h1>
             <div className='container2-skills'>
-                <div className='window'></div>
+                <div className='window'>
+                    <div className='head-window'>
+                        <span className='btn-red'></span>
+                        <span className='btn-yellow'></span>
+                        <span className='btn-green'></span>
+                        <p>pitt1dev@admin: ~</p>
+                    </div>
+                    <p></p>
+                </div>
                 <div className='carousel'></div>
             </div>
         </section>
