3feed0f934c67d88db0443637a4563b2d61fe7ee
PITT1 <petroleo.plta.97@gmail.com>
2024-06-23 21:08:56 -0400

    header listo :D

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,25 @@
-import '../styles/style.css'
+import { useState } from 'react'
+import '../styles/header-styles.css'
 const Header = () => {
+    const [dinamiClass, setDinamiClass] = useState("list");
+    const [xClass, setXClass] = useState("burguer");
+
+    const openList = () => {
+        if (dinamiClass === "list") {
+            setDinamiClass("list list-open");
+            setXClass("burguer-inner burguer-inner-x");    
+        } else {
+            setDinamiClass("list");
+            setXClass("burguer-inner");
+        }
+    }
     return(
         <header className='head'>
         <div>
             <h1>Pitt1<strong className='strong'>Dev</strong></h1>
         </div>
 
-        <div className='list'>
+        <div className={dinamiClass}>
             <ul>
                 <li>Start</li>
                 <li>Skills</li>
@@ -16,8 +29,8 @@ const Header = () => {
             </ul>
         </div>
 
-        <div className="burguer">
-            <span className="burguer-inner"></span>
+        <div className="burguer" onClick={openList}>
+            <span className={xClass}></span>
         </div>
 
         </header>
