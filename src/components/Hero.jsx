b204b30e281495690a09e618060ea37c271b4ec8
PITT1 <petroleo.plta.97@gmail.com>
2024-06-24 10:55:26 -0400

    hero casi listo solo falta responsividad

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import '../styles/hero-styles.css';
+
+const Hero = () => {
+    return(
+        <section className='container'>
+            <div className='presentation'>
+                <h1>Hi! I'm Pitt1<strong className='dev'>Dev</strong></h1>
+                <p>≥ FullStack developer in <strong className='strong-color'>Venezuela</strong></p>
+            </div>
+            <div className='image-container'>
+                <div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Hero;
