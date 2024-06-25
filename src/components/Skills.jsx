a3c8bc2f0d4fc61d4e097ab7d330bd7933a08539
PITT1 <petroleo.plta.97@gmail.com>
2024-06-24 20:26:26 -0400

    ventana de mac lista, falta el carousel

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -12,7 +12,9 @@ const Skills = () => {
                         <span className='btn-green'></span>
                         <p>pitt1dev@admin: ~</p>
                     </div>
-                    <p></p>
+                    <p>≥ Strong foundation in <strong className='light-letters'>semantic HTML,Javascript, React, and Astro; CSS, Styled Components, Bootstrap, Tailwind, FastApi with Python. I also have the basics of MySQL, PHP, and Laravel for the development of apis.</strong></p>
+                    <p>≥ I am always trying to keep myself constantly learning in both front-end and back-end development.</p>
+                    <button className='btn1'>Download CV</button>
                 </div>
                 <div className='carousel'></div>
             </div>
