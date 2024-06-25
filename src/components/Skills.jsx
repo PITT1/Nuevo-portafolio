e7e02cd43647601c89045a769ad86801d2778c9f
PITT1 <petroleo.plta.97@gmail.com>
2024-06-24 21:45:14 -0400

    tratando de hacer el carousel

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,15 @@
 import '../styles/skills-styles.css';
+import astroIcon from '../../src/images/astro-icon.png';
+import htmlIcon from '../images/html-icon.png';
+import cssIcon from '../images/css-icon.png';
+import reactIcon from '../images/react-icon.png';
+import pythonIcon from '../images/python-icon.png';
+import javaScriptIcon from '../images/js-icon.png';
+import tailWindIcon from '../images/tailwind-icon.png';
 
 const Skills = () => {
+    const imagesCarousel = [astroIcon, htmlIcon, cssIcon, reactIcon, pythonIcon, javaScriptIcon, tailWindIcon];
+
     return(
         <section className='container-skills'>
             <h1>Skills</h1>
@@ -16,7 +25,11 @@ const Skills = () => {
                     <p>≥ I am always trying to keep myself constantly learning in both front-end and back-end development.</p>
                     <button className='btn1'>Download CV</button>
                 </div>
-                <div className='carousel'></div>
+                <div className='carousel'>
+                    {imagesCarousel.map((image, index) => (
+                        <img src={image} alt={`Skill ${index + 1}`} key={index} height={96} width={96} />
+                    ))}
+                </div>
             </div>
         </section>
     )
