825514a68b8387c06ac22b02d2e6c088b3cb425f
PITT1 <petroleo.plta.97@gmail.com>
2024-06-25 11:14:08 -0400

    animando el window con framer motion

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,12 +1,13 @@
 import '../styles/skills-styles.css';
+import { motion } from 'framer-motion';
 
 const Skills = () => {
 
     return(
         <section className='container-skills'>
-            <h1>Skills</h1>
+            <motion.h1 initial={{y: 400, opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{duration: 0.5}}>Skills</motion.h1>
             <div className='container2-skills'>
-                <div className='window'>
+                <motion.div initial={{opacity: 0, x: -500}} whileInView={{opacity: 1, x: 0}} transition={{duration: 1}} className='window'>
                     <div className='head-window'>
                         <span className='btn-red'></span>
                         <span className='btn-yellow'></span>
@@ -16,7 +17,7 @@ const Skills = () => {
                     <p>≥ Strong foundation in <strong className='light-letters'>semantic HTML,Javascript, React, and Astro; CSS, Styled Components, Bootstrap, Tailwind, FastApi with Python. I also have the basics of MySQL, PHP, and Laravel for the development of apis.</strong></p>
                     <p>≥ I am always trying to keep myself constantly learning in both front-end and back-end development.</p>
                     <button className='btn1'>Download CV</button>
-                </div>
+                </motion.div>
                 <div className='carousel'>
                     <span className='imagen-icon astro-icon'></span>
                     <span className='imagen-icon js-icon'></span>
