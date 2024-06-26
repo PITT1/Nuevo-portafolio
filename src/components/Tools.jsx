ae3d50312b477fc6e8a77badd71677f1462d38e6
PITT1 <petroleo.plta.97@gmail.com>
2024-06-26 19:45:57 -0400

    agregando iconos a tools

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -1,11 +1,24 @@
-import '../styles/tools-styles.css';
+import "../styles/tools-styles.css";
+import { motion } from "framer-motion";
 
 const Tools = () => {
-    return(
-        <section className='tools-section'>
-
-        </section>
-    )
-}
+  return (
+    <section className="tools-section">
+      <motion.h1
+        initial={{ opacity: 0, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+      >
+        Tools
+      </motion.h1>
+      <div>
+        <span className="icon xampp-icon"></span>
+        <span className="icon visual-studio-code-icon"></span>
+        <span className="icon git-icon"></span>
+        <span className="icon postman-icon"></span>
+        <span className="icon figma-icon"></span>
+      </div>
+    </section>
+  );
+};
 
 export default Tools;
