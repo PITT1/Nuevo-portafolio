d8385dbb2b7aafc1dce12772f7a324ebb803ffbf
PITT1 <petroleo.plta.97@gmail.com>
2024-06-27 10:56:34 -0400

    correccion menor

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -10,28 +10,28 @@ const Tools = () => {
       >
         Tools
       </motion.h1>
-      <div>
+      <motion.div initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 1}}>
         <div>
-          <motion.span initial={{opacity: 0}} whileHover={{scale: 1.2}} whileInView={{opacity: 1}} transition={{duration: 1}} className="icon xampp-icon"></motion.span>
+          <motion.span whileHover={{scale: 1.2}} className="icon xampp-icon"></motion.span>
           <p className="nombre">XAMPP</p>
         </div>
         <div>
-          <motion.span initial={{opacity: 0}} whileHover={{scale: 1.2}} whileInView={{opacity: 1}} transition={{duration: 1}} className="icon visual-studio-code-icon"></motion.span>
+          <motion.span whileHover={{scale: 1.2}} className="icon visual-studio-code-icon"></motion.span>
           <p className="nombre">VS Code</p>
         </div>
         <div>
-          <motion.span initial={{opacity: 0}} whileHover={{scale: 1.2}} whileInView={{opacity: 1}} transition={{duration: 1}} className="icon git-icon"></motion.span>
+          <motion.span whileHover={{scale: 1.2}} className="icon git-icon"></motion.span>
           <p className="nombre">GIT</p>
         </div>
         <div>
-          <motion.span initial={{opacity: 0}} whileHover={{scale: 1.2}} whileInView={{opacity: 1}} transition={{duration: 1}} className="icon postman-icon"></motion.span>
+          <motion.span whileHover={{scale: 1.2}} className="icon postman-icon"></motion.span>
           <p className="nombre">Postman</p>
         </div>
         <div>
-          <motion.span initial={{opacity: 0}} whileHover={{scale: 1.2}} whileInView={{opacity: 1}} transition={{duration: 1}} className="icon figma-icon"></motion.span>
+          <motion.span whileHover={{scale: 1.2}} className="icon figma-icon"></motion.span>
           <p className="nombre">Figma</p>
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 };
