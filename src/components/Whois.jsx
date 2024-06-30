226e3fab1b4e7e07ec067a7e5e01f39686cd403d
PITT1 <petroleo.plta.97@gmail.com>
2024-06-30 12:30:39 -0400

    empezando con la seccion whois

---
+++ src/components/Whois.jsx
@@ -0,0 +1,18 @@
+import '../styles/whois-styles.css'
+
+const Whois = () => {
+    return(
+        <section className='whois-section'>
+            <h1>Who is Pitt1<strong>Dev?</strong></h1>
+            <div className='whois-content'>
+                <div>
+                    <div className='image'></div>
+                    <h2>Pitt1Dev</h2>
+                    <p>Full-Stack Developer</p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Whois;
