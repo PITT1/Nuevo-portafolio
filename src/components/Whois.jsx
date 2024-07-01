d748e82e6d8315aa6ecf4ff276a27ad01b4df87b
PITT1 <petroleo.plta.97@gmail.com>
2024-07-01 16:42:19 -0400

    whois casi lista

--- src/components/Whois.jsx
+++ src/components/Whois.jsx
@@ -11,6 +11,25 @@ const Whois = () => {
                     <h2>Pitt1Dev</h2>
                     <p>Full-Stack Developer</p>
                 </div>
+
+                <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          className="window"
+        >
+          <div className="head-window">
+            <span className="btn-red"></span>
+            <span className="btn-yellow"></span>
+            <span className="btn-green"></span>
+            <p>pitt1dev@admin: ~</p>
+          </div>
+          <p><strong className='green-letter'>≥</strong> Pedro Leon Torrez</p>
+          <p><strong className='green-letter'>≥</strong> I really like <strong className='light-letters'>JavaScript</strong> and <strong className='light-letters'>Python</strong> and I am always trying to learn new technologies that derive from those languages ​​to expand my resume.</p>
+          <p><strong className='green-letter'>≥</strong> I study <strong className='light-letters'>PHP</strong> only to use <strong className='light-letters'>Laravel</strong> 😂</p>
+          <p><strong className='green-letter'>≥</strong> I really love what I do! 💙 I think combining creativity and code can improve user experiences.</p>
+          <button className="btn1">see more</button>
+        </motion.div>
             </div>
         </section>
     )
