d708dcf7be69ff55800b0cfe6ba0d68829dc2b3e
PITT1 <petroleo.plta.97@gmail.com>
2024-07-01 10:17:50 -0400

    avanzando en la seccion whois

--- src/components/Whois.jsx
+++ src/components/Whois.jsx
@@ -1,11 +1,12 @@
 import '../styles/whois-styles.css'
+import { motion } from 'framer-motion';
 
 const Whois = () => {
     return(
         <section className='whois-section'>
-            <h1>Who is Pitt1<strong>Dev?</strong></h1>
+            <motion.h1 initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 3}}>Who is Pitt1<strong>Dev?</strong></motion.h1>
             <div className='whois-content'>
-                <div>
+                <div className='content-extern'>
                     <div className='image'></div>
                     <h2>Pitt1Dev</h2>
                     <p>Full-Stack Developer</p>
