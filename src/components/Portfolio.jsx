a8d1e8ff3a5c861e6a501a4da86341c2f635c88e
PITT1 <petroleo.plta.97@gmail.com>
2024-07-04 19:41:08 -0400

    empezando con la seccion de proyectos

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,12 @@
+import '../styles/portfolio-styles.css'
+
+const Portfolio = () => {
+    return(
+        <section className="portFolio-section">
+            <h1>Portfolio</h1>
+
+        </section>
+    )
+}
+
+export default Portfolio;
