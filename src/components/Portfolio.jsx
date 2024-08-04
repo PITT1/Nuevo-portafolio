8d95dccfec8e741faa04dd51c96b194aa9aa54a3
PITT1 <petroleo.plta.97@gmail.com>
2024-08-04 18:11:10 -0400

    creacion de componente proyectLink

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,12 +1,13 @@
-import '../styles/portfolio-styles.css'
+import '../styles/portfolio-styles.css';
 import ProjectLink from './ProjectLink';
+import wordgameImage from '../images/wordgame.png';
 
 const Portfolio = () => {
     return(
         <section className="portfolio-section" id='PORTFOLIO'>
             <h1>Portfolio</h1>
             <div className='contenedor'>
-                <ProjectLink/>
+                <ProjectLink title='juego de palabras' image={wordgameImage} altImage='proyecto de juego de palabras'/>
             </div>
 
         </section>
--- src/components/ProjectLink.jsx
+++ src/components/ProjectLink.jsx
@@ -1,10 +1,24 @@
+import PropTypes from "prop-types";
+import '../styles/projectlink-styles.css';
 
-const ProjectLink = () => {
+const ProjectLink = ({ title, image, altImage }) => {
     return(
-        <div>
-            
-        </div>
+        <figure className="proyecto-figure">
+            <div className="project-image">
+                <img src={image} alt={altImage}/>
+            </div>
+            <h3>{title}</h3>
+            <div className="tech">
+
+            </div>
+        </figure>
     )
 }
+
+ProjectLink.propTypes = {
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    altImage: PropTypes.string.isRequired
+  };
 
 export default ProjectLink;
