1991f7576c80bc3ea7fdb8d3820f38ed0fe28634
PITT1 <petroleo.plta.97@gmail.com>
2024-08-06 19:26:53 -0400

    agregando links a los proyectos

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,15 +8,18 @@ const Portfolio = () => {
             <div className='contenedor'>
             <figure className="proyecto-figure">
                 <div className="project-image wordgame"></div>
-                <h3>Juego de palabras</h3>
+                <a className='project-link' href="https://wordgamepitt1.netlify.app/"><h3>Juego de palabras</h3></a>
+                <a href='https://github.com/PITT1/WordGame' className='github-link'></a>
             </figure>
             <figure className="proyecto-figure">
                 <div className="project-image wheaterapp"></div>
-                <h3>app de clima</h3>
+                <a className='project-link' href="https://weatherpitt1.netlify.app/"><h3>app de clima</h3></a>
+                <a href='https://github.com/PITT1/weather-app' className='github-link'></a>
             </figure>
             <figure className="proyecto-figure">
                 <div className="project-image todoapp"></div>
-                <h3>app de tareas</h3>
+                <a className='project-link' href="https://todo-app-pitt1.netlify.app/"><h3>app de tareas</h3></a>
+                <a href='https://github.com/PITT1/TODO-app-frontendmentor' className='github-link'></a>
             </figure>
             </div>
         </section>
